f29c95e83b5c333c5f50fbb872f9d2ed2239feae
wonik <jowonik@mobinoma.com>
2024-02-26 09:39:42 +0900

    init

---
+++ src/main/java/com/walter/teamcitycicd/TeamcityCicdApplication.java
@@ -0,0 +1,13 @@
+package com.walter.teamcitycicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamcityCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walter/teamcitycicd/TeamcityCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walter.teamcitycicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcityCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
